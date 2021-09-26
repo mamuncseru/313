6b3e0c8903fd4177c8bd380394bacf6a31512a32
mamuncseru <mamun.cseian@gmail.com>
2021-09-26 16:05:16 +0600

    added satelite_data function

--- spaceapp/views.py
+++ spaceapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
+from threading import Timer
 # from rest_framework import viewsets
 # import requests
 
@@ -7,61 +9,41 @@ from skyfield.api import load, wgs84
 from skyfield.api import EarthSatellite
 import json
 
-stations_url = 'https://celestrak.com/NORAD/elements/1999-025.txt'
-# textfile = requests.get(stations_url)
-satellites = load.tle_file(stations_url)
-# print('Loaded', len(satellites), 'debris')
+def satelite_data():
+    print("function called....")
+    stations_url = 'https://celestrak.com/NORAD/elements/1999-025.txt'
+    # textfile = requests.get(stations_url)
+    satellites = load.tle_file(stations_url)
 
-# response_data = {}
-# i = 0
-# ts = load.timescale()
-# t = ts.now()
-# for sat in satellites:
-#     response_data[i] = f"{str(sat)}" + " " + f"{str(sat.at(t).position.km)}"
-#     i = i+1
-#     print(str(sat) + " " + str(sat.at(t).position.km))
+    ts = load.timescale()
+    t = ts.now()
 
+    # i = 0
+    sat_data = {}
+    longitude = []
+    latitude = []
+    name = []
+    epoch = []
+    for sat in satellites:
+        geocentric = sat.at(t)
+        subpoint = wgs84.subpoint(geocentric)
+        latitude.append(subpoint.latitude.degrees)
+        longitude.append(subpoint.longitude.degrees)
+        name.append(sat.name)
+        temp = str(sat).split(' ')
+        epoch.append(str(temp[6] + " " + temp[7]))
 
-ts = load.timescale()
-t = ts.now()
-
-# i = 0
-sat_data = {}
-longitude = []
-latitude = []
-name = []
-epoch = []
-for sat in satellites:
-    geocentric = sat.at(t)
-    # print(sat.name)
-    subpoint = wgs84.subpoint(geocentric)
-    # print(sat.epoch)
-    latitude.append(subpoint.latitude.degrees)
-    longitude.append(subpoint.longitude.degrees)
-    name.append(sat.name)
-    temp = str(sat).split(' ')
-    epoch.append(str(temp[6] + " " + temp[7]))
-    # this_latitude = subpoint.latitude.degrees
-    # this_longitude = subpoint.longitude.degrees
-    # print('Latitude:', this_latitude)
-    # print('Longitude:', this_longitude)
-    # print('Height: {:.1f} km'.format(subpoint.elevation.km))
-    # sat_data[i] = {
-    #         'Latitude': subpoint.latitude.degrees,
-    #         'Longitude': subpoint.longitude.degrees,
-    #         'epoch': str(sat.epoch),
-    #         'name': str(sat.name)
-    # }
-    # i+=1
-
-# sat_data_json = json.dumps(sat_data)
-sat_data["latitude"] = latitude
-sat_data["longitude"] = longitude
-sat_data["name"] = name
-sat_data["epoch"] = epoch
+    sat_data["latitude"] = latitude
+    sat_data["longitude"] = longitude
+    sat_data["name"] = name
+    sat_data["epoch"] = epoch
+    print(sat_data["latitude"][0])
+    return sat_data
 
 # Create your views here.
-
+run = True
 def index(request):
-    return HttpResponse(json.dumps(sat_data))
+    data = json.dumps(satelite_data())
+    return HttpResponse(data)
+
 
