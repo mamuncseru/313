7defe0214cc16451d64f3d4afb236c3fcb3af228
mamuncseru <mamun.cseian@gmail.com>
2021-09-26 16:18:35 +0600

    4 debris implemented

--- spaceapp/urls.py
+++ spaceapp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views;
 
 urlpatterns = [
-    path("data/", views.index)
+    path("1/", views.debri_1),
+    path("2/", views.debri_2),
+    path("3/", views.debri_3),
+    path("4/", views.debri_4)
 ]
--- spaceapp/views.py
+++ spaceapp/views.py
@@ -9,9 +9,7 @@ from skyfield.api import load, wgs84
 from skyfield.api import EarthSatellite
 import json
 
-def satelite_data():
-    print("function called....")
-    stations_url = 'https://celestrak.com/NORAD/elements/1999-025.txt'
+def satelite_data(stations_url):
     # textfile = requests.get(stations_url)
     satellites = load.tle_file(stations_url)
 
@@ -22,28 +20,53 @@ def satelite_data():
     sat_data = {}
     longitude = []
     latitude = []
-    name = []
-    epoch = []
+    altitude = []
+    name = ""
+    # epoch = []
     for sat in satellites:
         geocentric = sat.at(t)
         subpoint = wgs84.subpoint(geocentric)
         latitude.append(subpoint.latitude.degrees)
         longitude.append(subpoint.longitude.degrees)
-        name.append(sat.name)
+        name = sat.name
         temp = str(sat).split(' ')
-        epoch.append(str(temp[6] + " " + temp[7]))
+        # epoch.append(str(temp[6] + " " + temp[7]))
+        altitude.append(format(subpoint.elevation.km))
+
 
     sat_data["latitude"] = latitude
     sat_data["longitude"] = longitude
     sat_data["name"] = name
-    sat_data["epoch"] = epoch
-    print(sat_data["latitude"][0])
+    sat_data["altitude"] = altitude
+
+    # sat_data["epoch"] = epoch
+    # print(sat_data["latitude"][0])
     return sat_data
 
 # Create your views here.
-run = True
-def index(request):
-    data = json.dumps(satelite_data())
+url_1 = 'https://celestrak.com/NORAD/elements/1999-025.txt'
+url_2 = 'https://celestrak.com/NORAD/elements/2019-006.txt'
+url_3 = 'https://celestrak.com/NORAD/elements/iridium-33-debris.txt'
+url_4 = 'https://celestrak.com/NORAD/elements/cosmos-2251-debris.txt'
+
+
+def debri_1(request):
+    data = json.dumps(satelite_data(url_1))
+    return HttpResponse(data)
+
+
+def debri_2(request):
+    data = json.dumps(satelite_data(url_2))
+    return HttpResponse(data)
+
+
+def debri_3(request):
+    data = json.dumps(satelite_data(url_3))
+    return HttpResponse(data)
+
+
+def debri_4(request):
+    data = json.dumps(satelite_data(url_4))
     return HttpResponse(data)
 
 
