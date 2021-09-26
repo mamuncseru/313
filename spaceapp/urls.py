cec1c5a736033eba01b168b36ac281eb01813c73
mamuncseru <mamun.cseian@gmail.com>
2021-09-26 20:48:06 +0600

    prediction done

--- spaceapp/urls.py
+++ spaceapp/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("1/", views.debri_1),
     path("2/", views.debri_2),
     path("3/", views.debri_3),
-    path("4/", views.debri_4)
+    path("4/", views.debri_4),
+    path("p1/", views.pred_1),
+    path("p2/", views.pred_2),
+    path("p3/", views.pred_3),
+    path("p4/", views.pred_4),
 ]
--- spaceapp/views.py
+++ spaceapp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import time
-from threading import Timer
+from datetime import datetime, timezone
+import pytz
+import pandas as pd
+
 # from rest_framework import viewsets
 # import requests
 
 from skyfield.api import load, wgs84
-from skyfield.api import EarthSatellite
+# from skyfield.api import EarthSatellite
 import json
 
 def nan_checker(abc):
@@ -43,6 +45,8 @@ def satelite_data(stations_url):
     sat_data["longitude"] = longitude
     sat_data["name"] = name
     sat_data["altitude"] = altitude
+    print(max([float(x) for x in altitude]))
+    print(min([float(x) for x in altitude]))
 
     # sat_data["epoch"] = epoch
     # print(sat_data["latitude"][0])
@@ -53,6 +57,48 @@ url_1 = 'https://celestrak.com/NORAD/elements/1999-025.txt'
 url_2 = 'https://celestrak.com/NORAD/elements/2019-006.txt'
 url_3 = 'https://celestrak.com/NORAD/elements/iridium-33-debris.txt'
 url_4 = 'https://celestrak.com/NORAD/elements/cosmos-2251-debris.txt'
+
+
+
+def calculate_initial_paramenters(stations_url): 
+    # Load debris
+    satellites = load.tle_file(stations_url)
+
+    # Make a list of 100 days
+    today_date = datetime.now(timezone.utc)
+    today_date = today_date.replace(tzinfo=pytz.utc)
+    date_list = pd.date_range(start = today_date, periods = 5, freq='D').to_pydatetime().tolist()
+
+    # Load timescale
+    ts = load.timescale()
+    t = ts.utc(date_list)
+    #print(t)
+    # Generate a dictionary for satelite data
+    sat_data = {}
+    longitude = []
+    latitude = []
+    altitude = []
+    epoch = []
+
+    # Generate satelite data
+    i = 0
+    total_data = {}
+    for sat in satellites:
+        geocentric = sat.at(t)
+        # #print(geocentric)
+        subpoint = wgs84.subpoint(geocentric)
+        individual_data = {
+        "latitude" : [nan_checker(a) for a in (subpoint.latitude.degrees)],
+        "longitude" : [nan_checker(a) for a in (subpoint.longitude.degrees)],
+        "altitude" : [nan_checker(a) for a in (subpoint.elevation.km)]
+
+        }
+        total_data[i] = individual_data
+        i = i + 1
+    return total_data
+
+
+
 
 
 def debri_1(request):
@@ -74,4 +120,18 @@ def debri_4(request):
     data = json.dumps(satelite_data(url_4))
     return HttpResponse(data)
 
+def pred_1(request):
+    data = json.dumps(calculate_initial_paramenters(url_1))
+    return HttpResponse(data)
 
+def pred_2(request):
+    data = json.dumps(calculate_initial_paramenters(url_2))
+    return HttpResponse(data)
+
+def pred_3(request):
+    data = json.dumps(calculate_initial_paramenters(url_3))
+    return HttpResponse(data)
+
+def pred_4(request):
+    data = json.dumps(calculate_initial_paramenters(url_4))
+    return HttpResponse(data)
